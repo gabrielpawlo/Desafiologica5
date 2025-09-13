72746f40ea7499d54083ef8d965c6a3e6de781eb
Gabriel Pawlowski <gabrielgpawlowski@gmail.com>
2025-09-13 20:10:31 -0300

    botao sortear resolvido

--- js/app.js
+++ js/app.js
@@ -14,9 +14,23 @@ function adicionar(){
     nomeAmigo.value = '';
 }
 
-function sortear(){
+function sortear(){//em
+    let lista = document.getElementById('lista-sorteio');
     if(nomes.length === 0){
         alert('A lista está vazia. Adicione nomes antes de sortear.');
         return;
+    }
+    embaralhar(nomes);
+    let tamanho = nomes.length;
+    //lista.innerHTML = `<li>${nomes[0]} tirou ${nomes[1]}</li>`;
+    for(let i = 0; i < tamanho ; i++){
+        lista.innerHTML += `<li>${nomes[i]} tirou ${nomes[(i + 1) % tamanho]}</li>`;
+    }
+}
+
+function embaralhar(array){
+    for(let i = array.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
 }
